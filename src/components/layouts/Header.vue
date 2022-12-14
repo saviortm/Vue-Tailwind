3d2a922933b278bb28dfa09488984c6320263920
saviortm <106533562+saviortm@users.noreply.github.com>
2022-12-14 19:23:05 +0500

    Adding transitions to get started

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -30,7 +30,7 @@
     </div>
     <transition name="fade">
     <!-- Main modal -->
-    <div v-if="modal" id="authentication-modal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full flex backdrop-blur-sm ">
+    <div v-if="modal" id="authentication-modal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full flex backdrop-blur-sm">
       <div class="relative w-full h-full md:h-auto flex justify-center self-center">
         <!-- Modal content -->
 
